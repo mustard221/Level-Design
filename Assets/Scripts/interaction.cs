22b8570032e3e5b3ee81f9a1351e38d3695ab95b
mcsurvivalmode <C24385051@mytudublin.ie>
2025-04-14 14:52:53 +0100

    interactions added
    i have added the code for both the collectable interaction and the one that switches the scene!
    the pink one is the collectable one and the scene switcher is at the doorway up the top

---
+++ Assets/Scripts/interaction.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerInteraction : MonoBehaviour
+{
+    public UnityEvent enteredTrigger, exitedTrigger; //johns code from class. turns the object off once interacted!!
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        enteredTrigger.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        exitedTrigger.Invoke();
+    }
+}
